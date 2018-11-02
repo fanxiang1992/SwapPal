723b8e2dbff85dc0bcda9e6290350f4b301d8d1a
Jiangtao Xu <xujiangtao93@outlook.com>
2018-11-02 02:31:29 -0400

    finish profile page.

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -3,12 +3,13 @@ import {HomeComponent} from './components/home/home.component';
 import {ModuleWithProviders} from '@angular/core';
 import {LoginComponent} from './components/login/login.component';
 import {RegisterComponent} from './components/register/register.component';
-
+import {ProfileComponent} from './components/profile/profile.component';
 
 const APP_ROUTES: Routes = [
   {path: '', component : HomeComponent},
   {path: 'login', component: LoginComponent},
   {path: 'register', component: RegisterComponent},
+  {path: 'profile', component: ProfileComponent},
 ];
 
 // Export the routes as module providers
